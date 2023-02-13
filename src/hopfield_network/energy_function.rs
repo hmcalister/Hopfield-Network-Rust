ea2bbeeb60e072c189ab054de19e8d1504911352
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-02-13 14:31:59 +1300

    Update documentation to include return types

--- src/hopfield_network/energy_function.rs
+++ src/hopfield_network/energy_function.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use nalgebra::{DMatrix, DVector};
 
 /// Get the total energy of a state given a matrix representing a network 
@@ -6,6 +7,10 @@ use nalgebra::{DMatrix, DVector};
 /// 
 /// * `matrix` - The matrix representing the Hopfield Network
 /// * `vector` - The state to calculate the energy of
+/// 
+/// # Returns
+/// 
+/// An `f64` representing the overall energy of the state with respect to the matrix.
 pub fn state_energy_function(matrix: &DMatrix<f64>, vector: &DVector<f64>) -> f64 {
     // This is short hand to multiply take the sum of M_ij * V_i * V_j.
     // The first multiplication (matrix*vector) calculates M_ij * V_j.
@@ -20,10 +25,14 @@ pub fn state_energy_function(matrix: &DMatrix<f64>, vector: &DVector<f64>) -> f6
 /// 
 /// * `matrix` - The matrix representing the Hopfield Network
 /// * `vector` - The state to calculate the energy of
+/// 
+/// # Returns
+/// 
+/// A DVector of `f64` representing energies of each unit in the state with respect to the matrix.
 pub fn all_unit_energies(matrix: &DMatrix<f64>, vector: &DVector<f64>) -> DVector<f64>{
     (matrix * vector).scale(-1.0).component_mul(vector)
+    // (0..vector.len()).map(|i| unit_energy_function(matrix, vector, i)).collect()
 }
-
 /// Get the energy of a specific unit, returning this directly as an f64
 /// 
 /// # Arguments
@@ -31,6 +40,10 @@ pub fn all_unit_energies(matrix: &DMatrix<f64>, vector: &DVector<f64>) -> DVecto
 /// * `matrix` - The matrix representing the Hopfield Network
 /// * `vector` - The state to calculate the energy of
 /// * `index` - The index of the unit to calculate the energy for
+/// 
+/// # Returns
+/// 
+/// An `f64` representing the energy of the index in question.
 pub fn unit_energy_function(matrix: &DMatrix<f64>, vector: &DVector<f64>, index: usize) -> f64 {
     // This is much the same as the StateEnergyFunction but now only multiplies
     // the target rows together - hopefully saving cycles?
