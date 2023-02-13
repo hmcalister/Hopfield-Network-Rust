2c160792156ec4398abfef38abf81cb94ad0b498
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-02-13 14:33:02 +1300

    Update main with more testing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
 mod hopfield_network;
 
+use std::time::Instant;
+
 use crate::hopfield_network::*;
 
-const DIMENSION: usize = 10;
+const DIMENSION: usize = 100;
 const DOMAIN: NetworkDomain = NetworkDomain::BinaryDomain;
 
 fn main() {
-    let network = HopfieldNetworkBuilder::new_hopfield_network_builder()
+    let mut network = HopfieldNetworkBuilder::new_hopfield_network_builder()
         .set_network_dimension(DIMENSION)
         .set_network_domain(DOMAIN)
         .set_rand_matrix_init(false)
         .build();
 
-    let state_generator_builder = StateGeneratorBuilder::new_state_generator_builder()
-        .set_dimension(DIMENSION)
-        .set_domain(DOMAIN);
+    let state_generator_builder =
+        state_generator::StateGeneratorBuilder::new_state_generator_builder()
+            .set_dimension(DIMENSION)
+            .set_domain(DOMAIN);
 
     let mut state_generator = state_generator_builder.build();
 
-    println!("{:#?}\n", state_generator_builder);
-    println!("{:#?}\n", state_generator);
-    let state_collection = state_generator.create_state_collection(10);
-    for state in state_collection {
-        println!("{} {}", state, state.fold(0.0, |acc, i| acc + i));
+    
+    let now = Instant::now();
+    let states = state_generator.create_state_collection(10000);
+    for state in states{
+        network.relax_state(state);
     }
-
-    println!("{}", network);
+    println!("{}", now.elapsed().as_nanos());
+    
+    let now = Instant::now();
+    let states = state_generator.create_state_collection(10000);
+    network.concurrent_relax_state_collection(states, 8);
+    println!("{}", now.elapsed().as_nanos());
 }
