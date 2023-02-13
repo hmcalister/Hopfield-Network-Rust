9f516c417715ee75c74ab8011a3250d63f330609
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-02-13 14:31:34 +1300

    Update documentation to a large degree

--- src/hopfield_network/state_generator/mod.rs
+++ src/hopfield_network/state_generator/mod.rs
@@ -1,6 +1,8 @@
 pub mod state_generator_builder;
 
-use super::super::{ActivationFunction, NetworkDomain};
+pub use state_generator_builder::StateGeneratorBuilder;
+
+use super::super::{activation_function::ActivationFunction, NetworkDomain};
 use nalgebra::DVector;
 use rand::{rngs::StdRng, Rng};
 use rand_distr::Uniform;
@@ -15,8 +17,31 @@ pub struct StateGenerator {
     domain: NetworkDomain,
 }
 
+#[allow(dead_code)]
 impl StateGenerator {
+    /// Returns the RNG seed used to create this generator, for repetition.
+    /// 
+    /// # Returns
+    /// 
+    /// The seed of this state generator as a `u64`.
+    pub fn get_rng_seed(self: &StateGenerator) -> u64 {
+        self.rng_seed
+    }
+
+    /// Returns the domain of this state generator.
+    /// 
+    /// # Returns
+    /// 
+    /// The domain of this state generator as a `NetworkDomain`
+    pub fn get_domain(self: &StateGenerator) -> NetworkDomain {
+        self.domain
+    }
+
     /// Create a new state form the generator
+    /// 
+    /// # Returns
+    /// 
+    /// A single state from this generator as a `DVector<f64>` - already mapped by the activation function.
     pub fn next_state(self: &mut StateGenerator) -> DVector<f64> {
         let vector = DVector::<f64>::from_iterator(
             self.dimension,
@@ -27,6 +52,10 @@ impl StateGenerator {
     }
 
     /// Create a number of new states - returning this as a vector of DVectors
+    /// 
+    /// # Returns
+    /// 
+    /// A collection of states from this generator wrapped as a Vec.
     pub fn create_state_collection(
         self: &mut StateGenerator,
         num_states: usize,
